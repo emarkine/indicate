a232d087a0ccfb70becd463f846571abdebba9fe
Eugene <emarkine@gmail.com>
2018-06-21 14:49:31 +0200

    2018-06-21.14:49

--- bin/screen.usb.sh
+++ bin/screen.usb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 echo screen usb start
-sudo minicom -D /dev/ttyACM1 -b 9600 -C aaa.txt
+#sudo minicom -D /dev/ttyACM1 -b 9600 -C aaa.txt
 #getty tty
 #screen /dev/ttyACM0 
-#cat /dev/ttyACM0
+cat /dev/ttyACM0
 echo screen usb stop
